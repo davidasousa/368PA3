aa3af3c4e5d605bb9450f4fa63cc6b3f41b42d2f
David Alexander Sousa <dsousa@eceprog2.ecn.purdue.edu>
2023-03-31 19:08:04 -0400

    hi

--- pa3.h
+++ pa3.h
@@ -7,22 +7,39 @@ typedef struct _TreeNode {
     struct _TreeNode* left;
     struct _TreeNode* right;
     enum type{SPLIT,BOX} type;
-    void* info;
+    char split_type;
+    int box_num;
+    int width;
+    int height;
+    int xdim;
+    int ydim;
 } TreeNode;
 
 typedef struct _StackNode {
     struct _StackNode* prev;
-    void* info;
+    TreeNode* TNode;
 } StackNode;
 
-typedef struct _Split {
-    char split_type;
-} Split;
-
-typedef struct _Box {
-    int box_num;
-    int width;
-    int height;
-} Box;
-
 void create_stack(StackNode** stack_top, FILE* fp);
+
+TreeNode* stack_to_tree(StackNode** stack_top);
+
+// By here the stack has been destroyed and the tree has been fully  generated
+
+TreeNode* reroot_right_left(TreeNode* root);
+
+TreeNode* reroot_left_right(TreeNode* root);
+
+TreeNode* reroot_right_right(TreeNode* root);
+
+TreeNode* reroot_left_left(TreeNode* root);
+
+TreeNode* copy_tree(TreeNode* root);
+
+TreeNode* make_output1(TreeNode* root, FILE* fp);
+
+TreeNode* make_output2(TreeNode* root, FILE* fp);
+
+void find_reroot_dim(TreeNode* root, bool* is_left);
+
+TreeNode* make_output3(TreeNode* root, FILE* fp);
